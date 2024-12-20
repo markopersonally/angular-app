a96f76dd78865b3554ce76d717f57c65c8f84a91
mkpage1337 <mrku1337@gmail.com>
2024-12-20 16:47:33 +0100

    commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,12 +6,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+  imagePath = computed(() => '../../assets/users/' + this.avatar());
 
-  get imagePath() {
-    return '../../assets/users/' + this.avatar;
-  }
+  // get imagePath() {
+  //   return '../../assets/users/' + this.avatar;
+  // }
 
   onSelectUser() {}
 }
