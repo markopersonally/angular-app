0b80795e3106a2cd028b54bed866dbebb03063f3
mkpage1337 <mrku1337@gmail.com>
2024-12-25 20:09:58 +0100

    commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,6 +8,18 @@ import {
   Output,
 } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -17,11 +29,7 @@ export class UserComponent {
   // @Input({required: true}) avatar!: string;
   // @Input({required: true}) name!: string;
 
-  @Input({ required: true }) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
