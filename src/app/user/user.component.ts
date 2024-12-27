5566882fae8c7388f68e5c6d8d7f40696ded5c35
mkpage1337 <mrku1337@gmail.com>
2024-12-27 21:21:49 +0100

    commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,18 +7,13 @@ import {
   output,
   Output,
 } from '@angular/core';
+import { User } from './user.model';
 
 // type User = {
 //   id: string;
 //   avatar: string;
 //   name: string;
 // };
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
 
 @Component({
   selector: 'app-user',
