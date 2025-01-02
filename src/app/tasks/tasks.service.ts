16965ae8250debdce626f005272489ede4530ba8
mkpage1337 <mrku1337@gmail.com>
2025-01-02 16:32:59 +0100

    commit

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { type NewTaskData } from './task/task.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class TasksService {
   private tasks = [
     {
@@ -29,6 +29,14 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -45,5 +53,9 @@ export class TasksService {
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
