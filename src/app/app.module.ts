5985b67bed86488f1a4d1b1c10ebec631fd4df95
mkpage1337 <mrku1337@gmail.com>
2025-01-03 18:44:43 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent],
 })
 export class AppModule {}
